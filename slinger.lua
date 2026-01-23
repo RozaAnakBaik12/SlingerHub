05f0436a6157ac4e69633828eb361773603cef2e
RozaAnakBaik12 <razrza234@gmail.com>
2026-01-23 23:52:02 +0700

    slinger.lua

--- slinger.lua
+++ slinger.lua
@@ -1,49 +1,34 @@
--- Versi Ringan Khusus Delta Mobile
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Slinger Hub | Fish It!",
-   LoadingTitle = "Memuat Menu...",
+   LoadingTitle = "Delta Mobile Support",
    ConfigurationSaving = {Enabled = false}
 })
 
-local MainTab = Window:CreateTab("Auto Farm", 4483362458)
+local Tab = Window:CreateTab("Auto Farm", 4483362458)
 
-MainTab:CreateToggle({
-   Name = "Auto Cast (Lempar)",
+Tab:CreateToggle({
+   Name = "Auto Cast & Pull",
    CurrentValue = false,
    Callback = function(Value)
-      _G.AutoCast = Value
-      task.spawn(function()
-          while _G.AutoCast do
-              task.wait(1.5)
-              local char = game.Players.LocalPlayer.Character
-              local tool = char:FindFirstChildOfClass("Tool")
-              if tool then tool:Activate() end
-          end
-      end)
-   end,
-})
-
-MainTab:CreateToggle({
-   Name = "Auto Pull (Tarik)",
-   CurrentValue = false,
-   Callback = function(Value)
-      _G.AutoPull = Value
-      task.spawn(function()
-          while _G.AutoPull do
-              task.wait(0.2)
-              pcall(function()
-                  local gui = game.Players.LocalPlayer.PlayerGui
-                  for _, v in pairs(gui:GetDescendants()) do
-                      if v:IsA("TextButton") and v.Visible and (v.Text:lower():find("pull") or v.Text:lower():find("catch")) then
-                          local vim = game:GetService("VirtualInputManager")
-                          vim:SendMouseButtonEvent(v.AbsolutePosition.X + (v.AbsoluteSize.X/2), v.AbsolutePosition.Y + (v.AbsoluteSize.Y/2) + 54, 0, true, game, 1)
-                          vim:SendMouseButtonEvent(v.AbsolutePosition.X + (v.AbsoluteSize.X/2), v.AbsolutePosition.Y + (v.AbsoluteSize.Y/2) + 54, 0, false, game, 1)
-                      end
-                  end
-              end)
-          end
-      end)
+      _G.Fishing = Value
+      while _G.Fishing do
+         task.wait(0.5)
+         local tool = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+         if tool then
+            tool:Activate()
+         end
+         
+         -- Mencari tombol pull secara otomatis
+         local gui = game.Players.LocalPlayer.PlayerGui
+         for _, v in pairs(gui:GetDescendants()) do
+            if v:IsA("TextButton") and v.Visible and (v.Text:lower():find("pull") or v.Text:lower():find("catch")) then
+               local vim = game:GetService("VirtualInputManager")
+               vim:SendMouseButtonEvent(v.AbsolutePosition.X + (v.AbsoluteSize.X/2), v.AbsolutePosition.Y + (v.AbsoluteSize.Y/2) + 50, 0, true, game, 1)
+               vim:SendMouseButtonEvent(v.AbsolutePosition.X + (v.AbsoluteSize.X/2), v.AbsolutePosition.Y + (v.AbsoluteSize.Y/2) + 50, 0, false, game, 1)
+            end
+         end
+      end
    end,
 })
